1b3de2d37cbd7beee63a761f5a68ea1ba027e728
Pippo Pesic <philippesic06@gmail.com>
2023-01-30 13:38:52 -0800

    Added Week 3

---
+++ Week3/Programs/prog1/prog1.java
@@ -0,0 +1,8 @@
+package Programs.prog1;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Philip Pesic 2/6/23");
+    }
+}
---
+++ Week3/Programs/prog2/prog2.java
@@ -0,0 +1,9 @@
+package Programs.prog2;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("println");
+        System.out.print("print");
+        System.out.println("Philip Pesic 2/6/23");
+    }
+}
